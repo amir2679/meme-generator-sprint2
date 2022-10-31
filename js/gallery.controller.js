ef3bd7446fe3b8c0256c9851feffc237b54c4169
Amir Gombo <amir.gomb4@gmail.com>
2022-10-31 09:43:05 +0200

    small update 2.5

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -118,6 +118,8 @@ function onSetImgFilter(txt) {
 
 function onKeywordsClick(elKeyword) {
     const fontNumSize = +elKeyword.style.fontSize.split('px')[0]
+    gFilterByTxt = elKeyword.innerText
+    renderImgs()
     if(fontNumSize > 45)    return
     elKeyword.style.fontSize = `${fontNumSize * 1.1}px`
 }
